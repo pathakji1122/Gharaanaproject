bcb7827bb0307843f9cb5c06d96becd4f2e600da
prate <prateekprakash11@gmail.con>
2023-02-02 11:18:21 +0530

    responce

---
+++ gharaana/src/main/java/com/beginnner/gharaana/Service/DeleteCustomerResponce.java
@@ -0,0 +1,11 @@
+package com.beginnner.gharaana.Service;
+
+public class DeleteCustomerResponce {
+    public Boolean deleted;
+    public String responce;
+
+    public DeleteCustomerResponce(boolean deleted, String responce) {
+        this.deleted=deleted;
+        this.responce=responce;
+    }
+}
