59525741f95f0703dc9b3532288ef5f8ffd2f734
prate <prateekprakash11@gmail.con>
2023-02-02 12:20:24 +0530

    responce

--- gharaana/src/main/java/com/beginnner/gharaana/Service/OrderService.java
+++ gharaana/src/main/java/com/beginnner/gharaana/Service/OrderService.java
@@ -1,9 +1,6 @@
 package com.beginnner.gharaana.Service;
 
-import com.beginnner.gharaana.Entity.Customer;
-import com.beginnner.gharaana.Entity.Order;
-import com.beginnner.gharaana.Entity.OrderStatus;
-import com.beginnner.gharaana.Entity.Worker;
+import com.beginnner.gharaana.Entity.*;
 import com.beginnner.gharaana.Repo.CustomerRepository;
 import com.beginnner.gharaana.Repo.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,43 +15,55 @@ public class OrderService {
     UserService userService;
     @Autowired
     OrderRepository orderRepository;
-    public String createOrderId(OrderRequest orderRequest){
-        long id= orderRepository.count()+1;
+
+    public String createOrderId(OrderRequest orderRequest) {
+        long id = orderRepository.count() + 1;
         return String.valueOf(id);
 
     }
-    public void saveOrder(OrderRequest orderRequest,String orderId){
-        Customer customer=userService.getCustomerByToken(orderRequest.token);
-        OrderStatus orderStatus=OrderStatus.NOT_ACCEPTED;
-        Order order=new Order(customer.email,customer.name,customer.location,orderRequest.expertise,orderRequest.price,orderStatus,orderRequest.time,orderId,null);
+
+    public void saveOrder(OrderRequest orderRequest, String orderId) {
+        Customer customer = userService.getCustomerByToken(orderRequest.token);
+        OrderStatus orderStatus = OrderStatus.NOT_ACCEPTED;
+        Order order = new Order(customer.email, customer.name, customer.location, orderRequest.expertise, orderRequest.price, orderStatus, orderRequest.time, orderId, null);
         orderRepository.save(order);
     }
-    public Order acceptOrder(AcceptOrderRequest acceptOrderRequest){
-        Order order=orderRepository.findByOrderId(acceptOrderRequest.orderId);
-        String token=acceptOrderRequest.token;
-        Worker worker=userService.getWorkerByToken(token);
-        order.orderStatus=OrderStatus.ACCEPTED;
-        order.GharanaAgent= worker.email;
+
+    public Order acceptOrder(AcceptOrderRequest acceptOrderRequest) {
+        Order order = orderRepository.findByOrderId(acceptOrderRequest.orderId);
+        String token = acceptOrderRequest.token;
+        Worker worker = userService.getWorkerByToken(token);
+        order.orderStatus = OrderStatus.ACCEPTED;
+        order.GharanaAgent = worker.email;
         orderRepository.save(order);
         return order;
     }
-    public List<Order>checkOrders(CheckOrdersRequest checkOrdersRequest){
-        Worker worker=userService.getWorkerByToken(checkOrdersRequest.token);
-        List<Order>orders=orderRepository.findByLocationAndExpertise(worker.location,worker.expertise);
+
+    public List<Order> checkOrders(CheckOrdersRequest checkOrdersRequest) {
+        Worker worker = userService.getWorkerByToken(checkOrdersRequest.token);
+        List<Order> orders = orderRepository.findByLocationAndExpertise(worker.location, worker.expertise);
         return orders;
     }
-    public List<Order>myOrder(MyOrderReques myOrderReques){
-        String token= myOrderReques.token;
-        Customer customer=userService.getCustomerByToken(token);
+
+    public List<Order> myOrder(MyOrderReques myOrderReques) {
+        String token = myOrderReques.token;
+        Customer customer = userService.getCustomerByToken(token);
         return orderRepository.findByEmail(customer.email);
     }
-    public Order orderStatus(OrderStatusRequest orderStatusRequest){
-        Customer customer=userService.getCustomerByToken(orderStatusRequest.token);
 
-        Order order=orderRepository.findByOrderId(orderStatusRequest.orderId);
-        if(order.email.equals(customer.email)==false||order==null){
+    public Order orderStatus(OrderStatusRequest orderStatusRequest) {
+        Customer customer = userService.getCustomerByToken(orderStatusRequest.token);
+        Order order = orderRepository.findByOrderId(orderStatusRequest.orderId);
+        if (order.email.equals(customer.email) == false || order == null) {
             return null;
         }
         return order;
+    }
+    public Boolean expertiseAvail(String value){
+        Expertise  expertise =Expertise.checkExpertise(value);
+        if(expertise==null){
+            return false;
+        }
+        return true;
     }
 }
--- gharaana/src/main/java/com/beginnner/gharaana/Service/OrderStatusResponce.java
+++ gharaana/src/main/java/com/beginnner/gharaana/Service/OrderStatusResponce.java
@@ -4,7 +4,11 @@ import com.beginnner.gharaana.Entity.Order;
 
 public class OrderStatusResponce {
     public Order order;
-    public OrderStatusResponce(Order order) {
+    public Boolean status;
+    public String responce;
+    public OrderStatusResponce(Boolean status,String responce,Order order) {
+        this.status=status;
+        this.responce=responce;
         this.order=order;
     }
 }
