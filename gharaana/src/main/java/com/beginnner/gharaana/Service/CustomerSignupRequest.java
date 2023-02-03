839581ae82a9c01ae717a3ea85e3ae54da11166a
prate <prateekprakash11@gmail.con>
2023-02-03 17:04:25 +0530

    updated responce

--- gharaana/src/main/java/com/beginnner/gharaana/Service/CustomerSignupRequest.java
+++ gharaana/src/main/java/com/beginnner/gharaana/Service/CustomerSignupRequest.java
@@ -1,19 +1,18 @@
 package com.beginnner.gharaana.Service;
 
-import com.beginnner.gharaana.Entity.Expertise;
 import com.beginnner.gharaana.Entity.Location;
 import com.beginnner.gharaana.Entity.ServicePack;
 import org.springframework.lang.NonNull;
 
-public class SignupRequest {
+public class CustomerSignupRequest {
     @NonNull
     public String email;
     public String password;
     public String name;
-    public String phonenNo;
+    public String phoneNo;
    public Location location;
    public ServicePack servicePack;
-   public Expertise expertise;
+
 
 
 }
