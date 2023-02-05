f4541efd2be0318e87e88c6ccc86c15323797553
prate <prateekprakash11@gmail.con>
2023-02-05 14:29:04 +0530

    updated responce

---
+++ gharaana/src/main/java/com/beginnner/gharaana/Service/CompleteOrderResponce.java
@@ -0,0 +1,12 @@
+package com.beginnner.gharaana.Service;
+
+import com.beginnner.gharaana.Entity.Order;
+
+public class CompleteOrderResponce {
+    public boolean status;
+    public Order order;
+    public String responce;
+
+    public CompleteOrderResponce(String order_completed, boolean b) {
+    }
+}
